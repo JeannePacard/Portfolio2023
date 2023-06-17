895a46cd00f74830a69c6fd368b848aa52817122
JeannePacard <jeanne.pacard@gmail.com>
2023-06-17 18:05:31 +0200

    add works

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,5 +1,138 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Flicking from "@egjs/react-flicking";
+import { Perspective } from "@egjs/flicking-plugins";
 
-export default function Works() {
-  return <></>;
+function Slider() {
+  const flickingRef = useRef(null);
+
+  useEffect(() => {
+    if (flickingRef.current) {
+      flickingRef.current.addPlugins(new Perspective({ rotate: 0.5 }));
+    }
+  }, []);
+
+  return (
+    <div id="flick" className="flicking-viewport">
+      <div className="flicking-camera">
+        <Flicking ref={flickingRef} circular={true}>
+          <div className="card-panel">
+            <img src="./assets/Works/cattravel.png" alt="site web" />
+            <h1>assurever</h1>
+            <p>
+              language : React <br /> role : developer
+            </p>
+            <a
+              href="https://test-site-assurever-git-master-jeannepacard.vercel.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/cattravel.png" alt="site web" />
+            <h1>around the world</h1>
+            <p>
+              language : HTML / CSS / JS <br /> role : designer, developer
+            </p>
+            <a
+              href="https://jeannepacard.github.io/cattravel/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/sisi.png" alt="site web" />
+            <h1>portfolio</h1>
+            <p>
+              language : HTML / CSS <br /> role : developer
+            </p>
+            <a
+              href="https://sirinebenamour.github.io/portfolio/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/seo.png" alt="site web" />
+            <h1>escape game</h1>
+            <p>
+              language : HTML / CSS / JS <br /> role : designer, developer
+            </p>
+            <a
+              href="https://jeannepacard.github.io/cattravel/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/zurg.png" alt="site web" />
+            <h1>
+              zurg<span>'</span>s revenge
+            </h1>
+            <p>
+              language : JS <br /> role : designer, animator, developer
+            </p>
+            <a
+              href="https://jeannepacard.github.io/LaRevancheDeZurg/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/isis.png" alt="site web" />
+            <h1>isis</h1>
+            <p>
+              language : HTML / CSS / JS <br /> role : designer, developer
+            </p>
+            <a
+              href="https://jeannepacard.github.io/SiteIsis/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/krunks.png" alt="site web" />
+            <h1>portfolio</h1>
+            <p>
+              language : HTML / CSS <br /> role : developer
+            </p>
+            <a
+              href="https://kylian-brun.vercel.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+          <div className="card-panel">
+            <img src="./assets/Works/lena.png" alt="site web" />
+            <h1>portfolio</h1>
+            <p>
+              language : HTML / CSS <br /> role : developer
+            </p>
+            <a
+              href="https://lenabrincat.github.io/lenabrincatportfolio/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              view more
+            </a>
+          </div>
+        </Flicking>
+      </div>
+    </div>
+  );
 }
+
+export default Slider;
