0aea6b50bac5a39663866f4802b7e73dc8c77494
JeannePacard <jeanne.pacard@gmail.com>
2023-06-20 16:38:42 +0200

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export default function Contact() {
-  return <></>;
+  return (
+    <>
+      <div className="contact">
+        <div className="contact-content">
+          <h1>would you like to work with me ?</h1>
+          <p>Contact me :</p>
+          <br />
+          <a href="mailto:jeanne.pacard@gmail.com">jeanne.pacard@gmail.com</a>
+          <a
+            href="https://www.instagram.com/jeanneflammini.pro/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            instagram : jeanneflammini.pro
+          </a>
+          <a
+            href="https://github.com/JeannePacard"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github : Jeanne Pacard
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jeanne-pacard-flammini/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin : Jeanne Pacard Flammini
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
